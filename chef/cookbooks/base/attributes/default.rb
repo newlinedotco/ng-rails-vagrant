fadf37de5bbf818eeb1bff64dc1f4b992c9a36f5
Nate Murray <nate@natemurray.com>
2014-01-30 16:10:46 -0800

    fix the vagrant path with rvm

--- chef/cookbooks/base/attributes/default.rb
+++ chef/cookbooks/base/attributes/default.rb
@@ -1,11 +1,14 @@
+override['rvm']['vagrant']['system_chef_client'] = "/opt/chef/bin/chef-client"
+override['rvm']['vagrant']['system_chef_solo'] = "/opt/chef/bin/chef-solo"
 override['rvm']['user_installs'] = [
   { 
     'user'            => 'vagrant',
     'default_ruby'    => 'ruby-2.0.0-p195',
     'global_gems'     => [
       { 'name'    => 'bundler',
-        'version' => '1.5.2'
-      },
+        'version' => '1.5.2' },
+      { 'name'    => 'rails',
+        'version' => '4.0.2' },
       { 'name'    => 'rake' }
     ]
   }
--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -2,6 +2,7 @@ run_list [
   'recipe[apt]',
   'recipe[git]',
   'recipe[rvm::system]',
+  'recipe[rvm::vagrant]',
   'recipe[rvm::user]',
   'recipe[base]'
 ]
