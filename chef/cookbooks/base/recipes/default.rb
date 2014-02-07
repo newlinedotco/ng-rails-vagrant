b9d080579b271a54965e801c8296c083c34c11f5
Nate Murray <nate@natemurray.com>
2014-02-07 11:16:37 -0800

    Added phantom recipes

--- chef/cookbooks/base/recipes/default.rb
+++ chef/cookbooks/base/recipes/default.rb
@@ -6,9 +6,16 @@
 #
 package "tree"
 package "lynx-cur"
+package "tmux"
 include_recipe "rvm::user"
 
 npm_package "grunt-cli" do
   version "0.1.9"
   action :install
 end 
+
+# for phantom
+package "libfontconfig1" 
+package "fontconfig" 
+package "libfontconfig1-dev" 
+package "libfreetype6-dev"
